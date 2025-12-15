0dc303176839e2e03d0094b10a9cf00c313d94e3
Ghislain Vachet <ghislainvachet@yahoo.fr>
2025-12-15 14:41:18 +0100

    petite maj

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,17 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { About } from './about/about';
-import { TasksPage } from './features/tasks/tasks-page/tasks-page';
+
 
 export const routes: Routes = [
 
     { path:'', component:Home},
     { path:'about', component:About },
-    { path:'tasks', component: TasksPage }
 
-    // {
-    //     path: 'tasks',
-    //     loadChildren: () => 
-    //         import('./features/tasks/tasks-page/routes').then(m => m.TASKS_ROUTES)
-    // }
+    {
+        path: 'tasks',
+        loadChildren: () => 
+            import('./features/tasks/tasks-page/routes').then(m => m.TASKS_ROUTES)
+    }
 
 ];
